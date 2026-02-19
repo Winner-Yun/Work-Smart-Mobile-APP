d6faed6e79e5cb695c67e5641e1127feff83c7d3
thasopheak777-cpu <thasopheak777@gmail.com>
2026-02-19 09:12:46 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,29 @@
 <<<<<<< HEAD
-// import 'package:flutter/material.dart';
-// import 'package:flutter_worksmart_mobile_app/app/theme/theme.dart';
-// import 'package:flutter_worksmart_mobile_app/app/routes/app_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_worksmart_mobile_app/app/theme/theme.dart';
+import 'package:flutter_worksmart_mobile_app/app/routes/app_route.dart';
 
-// void main() {
-//   runApp(const MainApp());
-// }
+void main() {
+  runApp(const MainApp());
+}
 
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'WorkSmart',
-//       theme: AppThemes.lightTheme,
-//       darkTheme: AppThemes.darkTheme,
-//       themeMode: ThemeMode.system,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WorkSmart',
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
+      themeMode: ThemeMode.system,
 
-//       initialRoute: AppRoute.tutorial,
-//       routes: AppRoute.routes,
-//     );
-//   }
-// }
+      initialRoute: AppRoute.tutorial,
+      routes: AppRoute.routes,
+    );
+  }
+}
 
 import 'package:flutter/material.dart';
 import 'package:flutter_worksmart_mobile_app/app/theme/theme.dart';
